48e95e87b7a9f1e99bfa4f158dc37463fec7e953
Artem Vasilev <homelesspravitas@gmail.com>
2025-04-17 20:16:12 +0600

    Added and scaffolded a model.

---
+++ MovieDatabase/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieDatabase.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateOnly RealeseDate { get; set; }
+        public string? Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
--- MovieDatabase/Program.cs
+++ MovieDatabase/Program.cs
@@ -1,4 +1,7 @@
-using MovieDatabase.Components;
+﻿using MovieDatabase.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieDatabase.Data;
 
 namespace MovieDatabase
 {
@@ -7,6 +10,12 @@ namespace MovieDatabase
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContextFactory<MovieDatabaseContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDatabaseContext") ?? throw new InvalidOperationException("Connection string 'MovieDatabaseContext' not found.")));
+
+            builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
@@ -20,6 +29,7 @@ namespace MovieDatabase
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+    app.UseMigrationsEndPoint();
             }
 
             app.UseHttpsRedirection();
