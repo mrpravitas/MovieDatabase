125e6bc0687eacaa13bcc2a2b59cf1bc7de2bf7b
Artem Vasilev <homelesspravitas@gmail.com>
2025-04-22 17:39:27 +0600

    Added movie rating field.

---
+++ MovieDatabase/Migrations/20250422112409_MovieRatingField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieRatingField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "R");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
